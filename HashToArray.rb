19387bc6d465a14b641640cf4c80c2a6f08bfa4d
ArtiOdedara <arti.odedara@bacancy.com>
2023-03-01 16:11:37 +0530

    Convert hash into array

--- HashToArray.rb
+++ HashToArray.rb
@@ -1,17 +1,17 @@
 =begin
 Convert a hash into an array. Nothing more, Nothing less.
 =end
-class Main
+class HashToArray
   def hash_to_arr
-    hash = {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+    hash = { name: 'Jeremy', age: 24, role: 'Software Engineer'}
     arr = Array.new
     hash.each {
         |k,v| arr.push([k.to_s,v])
     }  
     puts "Input: #{hash}"
-    print "Output: #{arr}"
+    print "Output: #{arr.sort}"
   end
 end
 
-obj = Main.new
+obj = HashToArray.new
 obj.hash_to_arr
